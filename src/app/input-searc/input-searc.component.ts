fed22e017cfdb23f67d9a8c54e1c9e5436bcd8c6
MichaelShveyn <88609796+MichaelShveyn@users.noreply.github.com>
2024-08-24 19:13:05 +0300

    add main files for project setup

---
+++ src/app/input-searc/input-searc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-searc',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-searc.component.html',
+  styleUrl: './input-searc.component.css'
+})
+export class InputSearcComponent {
+
+}
---
+++ src/app/item-dialog/item-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-dialog.component.html',
+  styleUrl: './item-dialog.component.css'
+})
+export class ItemDialogComponent {
+
+}
